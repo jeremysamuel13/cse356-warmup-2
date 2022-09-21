fc058fd2fd1561288cae3943a2e62be29e93f8b7
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2022-09-21 12:59:34 -0400

    set form action and method

--- src/TTTForm.tsx
+++ src/TTTForm.tsx
@@ -3,7 +3,7 @@ import "./TTTForm.css"
 
 const TTTForm = () =>
     <div className='ttt-form-div'>
-        <form>
+        <form action="/ttt" method="get">
             <label htmlFor="name">{'Name: '}
                 <input type="text" id="name" name="name" />
             </label>
