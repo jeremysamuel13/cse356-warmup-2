70f1fcf67e935ad901ba89f0058e764a808bd123
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2022-09-21 00:25:21 -0400

    remove hydration

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement, <React.StrictMode>
-  <App isHome={true} name={"default"} date={new Date()} />
-</React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+
+root.render(<React.StrictMode>
+  <App isHome={true} name={"default"} date={new Date()} />
+</React.StrictMode>)
 
